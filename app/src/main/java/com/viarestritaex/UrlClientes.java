e0e329c172326559f62b9024418f01889ea5ce0b
Marcos <marcos@mappsistemas.com.br>
2017-09-05 13:50:16 -0300

    chaveMain3

--- app/src/main/java/com/viarestritaex/UrlClientes.java
+++ app/src/main/java/com/viarestritaex/UrlClientes.java
@@ -10,35 +10,8 @@ public class UrlClientes {
      * viarestritaex
      */
     public static String url = "http://viarestritaex.com.br/";
-    /**
-     * spartanmotoboys
-     */
-    //public static String url = "http://spartanmotoboys.com.br/";
-    /**
-     * zipmarvlog
-     */
-   // public static String url = "http://zipmarvlog.com.br/";
-    /**
-     * mexsolog
-     */
-    //public static String url = "http://mexsolog.com.br/";
-    /**
-     * movameapp
-     */
-    //public static String url = "http://movameapp.com.br/";
-    /**
-     * mototaxiprecojusto
-     */
-    //public static String url = "http://mototaxiprecojusto.com.br/";
-    /**
-     * entregaagora
-     */
-    //public static String url = "http://entregaagora.com/";
 
-    /**
-     * heldtransportes
-     */
-    //public static String url = "http://heldtransportes.com/";
+
 
     /**
      * mototaxionline
