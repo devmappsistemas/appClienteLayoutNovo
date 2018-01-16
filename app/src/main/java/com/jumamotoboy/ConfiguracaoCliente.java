f105fe71858601a2133557f28d00da6309890052
MarcosN <marcoslmai@gmail.com>
2018-01-16 08:48:34 -0200

    Commint projeto

---
+++ app/src/main/java/com/jumamotoboy/ConfiguracaoCliente.java
@@ -0,0 +1,11 @@
+package com.jumamotoboy;
+
+/**
+ * Created by DEV on 10/08/2016.
+ */
+public class ConfiguracaoCliente {
+
+   // public static String nomeCliente = "viarestritaex";
+    public static String versaoApp = "3.1";
+
+}
---
+++ app/src/main/java/com/jumamotoboy/UrlClientes.java
@@ -0,0 +1,23 @@
+package com.jumamotoboy;
+
+/**
+ * Created by User on 25/08/2017.
+ */
+
+public class UrlClientes {
+
+    /**
+     * url cliente
+     */
+    public static String url = "http://www.jumamotoboy.com.br/";
+
+
+
+    /**
+     * mototaxionline
+     */
+    //public static String url = "http://mototaxionline.com/";
+
+
+
+}
